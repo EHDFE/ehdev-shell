84503d15c16b15ddad5875d7c460bc8ffb969624
杰枫 <huanggbxjp@sohu.com>
2017-11-08 18:42:48 +0800

    complete versions table

--- app/components/component.dependencyManager/DependencyManager.js
+++ app/components/component.dependencyManager/DependencyManager.js
@@ -3,53 +3,108 @@ import PropTypes from 'prop-types';
 import styles from './index.less';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { Table } from 'antd';
+import { Table, Tabs, Button } from 'antd';
 
-const columns = [
-  {
-    title: 'package name',
-    dataIndex: 'packageName'
-  },
-  {
-    title: 'current',
-    dataIndex: 'current'
-  },
-  {
-    title: 'wanted',
-    dataIndex: 'wanted'
-  },
-  {
-    title: 'latest',
-    dataIndex: 'latest'
-  }
-];
+const TabPane = Tabs.TabPane;
 
 class DependencyManager extends Component {
   static propTypes = {
-    pkgInfo: PropTypes.object
+    pkgInfo: PropTypes.object,
+    pkg: PropTypes.object
+  };
+
+  state = {
+    dataSource: [],
+    tab: 'dependencies',
+    selectedRows: []
+  };
+
+  filterData = key => {
+    const { pkg, pkgInfo } = this.props;
+    const data = [];
+    for (let i in pkg[key]) {
+      data.push(Object.assign({ key: i, packageName: i }, pkgInfo.versions[i]));
+    }
+    this.setState({
+      dataSource: data,
+      tab: key
+    });
+  };
+  componentDidMount() {
+    this.filterData(this.state.tab);
   }
-  
+  componentWillReceiveProps() {
+    this.filterData(this.state.tab);
+  }
   render() {
-    const { pkgInfo } = this.props;
     const rowSelection = {
-      onChange: (selectedRowKeys, selectedRows) => {},
+      onChange: (selectedRowKeys, selectedRows) => {
+        this.setState({selectedRows});
+      },
       getCheckboxProps: record => ({
         disabled: record.name === 'Disabled User' // Column configuration not to be checked
       })
     };
-    const data = [];
-    for (let i in pkgInfo.versions) {
-      data.push(Object.assign({ key: i, packageName: i }, pkgInfo.versions[i]));
-    }
-
+    const columns = [
+      {
+        title: 'Package name',
+        dataIndex: 'packageName',
+        render: (text, record, index) => {
+          return record.outdated ? (
+            <span style={{ color: 'red' }}>{text}</span>
+          ) : (
+            <span style={{ color: '#108ee9' }}>{text}</span>
+          );
+        }
+      },
+      {
+        title: 'Current',
+        dataIndex: 'current'
+      },
+      {
+        title: 'Wanted',
+        dataIndex: 'wanted'
+      },
+      {
+        title: 'Latest',
+        dataIndex: 'latest'
+      },
+      {
+        title: '操作',
+        render: () => {
+          return (
+            <div>
+              <Button type="primary" style={{ marginRight: '20px' }}>
+                Update
+              </Button>
+              <Button type="danger">Delete</Button>
+            </div>
+          );
+        }
+      }
+    ];
     return (
-      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+      <div>
+        <Tabs onChange={this.filterData} type="card">
+          <TabPane tab="Dependencies" key="dependencies" />
+          <TabPane tab="Dev Dependencies" key="devDependencies" />
+        </Tabs>
+        <div style={{ marginBottom: '10px' }}>
+          <Button type="primary">
+            Batch Update
+          </Button>
+          <Button type="primary" style={{ float:'right' }}>
+            Add New Dependency
+          </Button>
+        </div>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={this.state.dataSource}
+        />
+      </div>
     );
-    // return (
-    //   <div>ddd</div>
-    // );
   }
 }
-
 
 export default DependencyManager;

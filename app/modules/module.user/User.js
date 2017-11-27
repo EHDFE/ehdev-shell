4e74d070e7daefa83f443d12fdf8b565d15d7b7e
ryan.bian <macisi528@gmail.com>
2017-11-27 18:35:19 +0800

    CHANGE: layout design

--- app/modules/module.user/User.js
+++ app/modules/module.user/User.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
-import { Form, Upload, Icon, message, Card, Input, Button, Row, Col } from 'antd';
+import { Form, Upload, Icon, message, Input, Button, Row, Col } from 'antd';
 
 import { actions } from './store';
 
@@ -89,75 +89,73 @@ class User extends Component {
 
     return (
       <Page>
-        <Form onSubmit={this.handleSubmit}>
-          <Card title="个人信息" className={styles.User__Card}>
-            <Row gutter={10}>
-              <Col span={16}>
-                <FormItem {...formItemLayout} label="你的昵称">
-                  {getFieldDecorator('name', {
-                    initialValue: name,
-                    rules: [{ required: true, message: '请输入你的昵称！' }],
-                  })(<Input placeholder="请输入你的昵称" />)}
-                </FormItem>
+        <Form onSubmit={this.handleSubmit} className={styles.User__Card}>
+          <Row gutter={10}>
+            <Col span={16}>
+              <FormItem {...formItemLayout} label="你的昵称">
+                {getFieldDecorator('name', {
+                  initialValue: name,
+                  rules: [{ required: true, message: '请输入你的昵称！' }],
+                })(<Input placeholder="请输入你的昵称" />)}
+              </FormItem>
 
-                <FormItem {...formItemLayout} label="你的github">
-                  {getFieldDecorator('github', {
-                    initialValue: github,
-                  })(<Input placeholder="请输入你的github地址" />)}
-                </FormItem>
+              <FormItem {...formItemLayout} label="你的github">
+                {getFieldDecorator('github', {
+                  initialValue: github,
+                })(<Input placeholder="请输入你的github地址" />)}
+              </FormItem>
 
-                <FormItem {...formItemLayout} label="你在哪里">
-                  {getFieldDecorator('address', {
-                    initialValue: address,
-                  })(<CityPicker placeholder='请选择所在城市'/>)}
-                </FormItem>
+              <FormItem {...formItemLayout} label="你在哪里">
+                {getFieldDecorator('address', {
+                  initialValue: address,
+                })(<CityPicker placeholder='请选择所在城市'/>)}
+              </FormItem>
 
-                <FormItem {...formItemLayout} label="你想说的">
-                  {getFieldDecorator('bio', {
-                    initialValue: bio,
-                  })(<Input.TextArea rows={4} placeholder="你想说什么都可以" />)}
-                </FormItem>
+              <FormItem {...formItemLayout} label="你想说的">
+                {getFieldDecorator('bio', {
+                  initialValue: bio,
+                })(<Input.TextArea rows={4} placeholder="你想说什么都可以" />)}
+              </FormItem>
 
-                <FormItem wrapperCol={{ span: 12, offset: 7 }}>
-                  <Button type="primary" htmlType="submit">
+              <FormItem wrapperCol={{ span: 12, offset: 7 }}>
+                <Button type="primary" htmlType="submit">
                     更新个人信息
-                  </Button>
-                </FormItem>
+                </Button>
+              </FormItem>
 
-              </Col>
-              <Col span={8}>
-                <FormItem help="点击上传新头像" style={{ textAlign: 'center' }}>
-                  {getFieldDecorator('avatar', {
-                    initialValue: avatar,
-                    valuePropName: 'file',
-                    getValueFromEvent: this.normFile,
-                  })(
-                    <Upload
-                      className={styles.User__avatar_uploader}
-                      name="avatar"
-                      showUploadList={false}
-                      action={FILE_PATH}
-                      beforeUpload={beforeUpload}
-                      // onChange={this.handleChange}
-                    >
-                      {imgUrl ? (
-                        <img
-                          src={imgUrl}
-                          alt=""
-                          className={styles.User__avatar}
-                        />
-                      ) : (
-                        <Icon
-                          type="plus"
-                          className={styles.User__avatar_uploader_trigger}
-                        />
-                      )}
-                    </Upload>
-                  )}
-                </FormItem>
-              </Col>
-            </Row>
-          </Card>
+            </Col>
+            <Col span={8}>
+              <FormItem help="点击上传新头像" style={{ textAlign: 'center' }}>
+                {getFieldDecorator('avatar', {
+                  initialValue: avatar,
+                  valuePropName: 'file',
+                  getValueFromEvent: this.normFile,
+                })(
+                  <Upload
+                    className={styles.User__avatar_uploader}
+                    name="avatar"
+                    showUploadList={false}
+                    action={FILE_PATH}
+                    beforeUpload={beforeUpload}
+                    // onChange={this.handleChange}
+                  >
+                    {imgUrl ? (
+                      <img
+                        src={imgUrl}
+                        alt=""
+                        className={styles.User__avatar}
+                      />
+                    ) : (
+                      <Icon
+                        type="plus"
+                        className={styles.User__avatar_uploader_trigger}
+                      />
+                    )}
+                  </Upload>
+                )}
+              </FormItem>
+            </Col>
+          </Row>
         </Form>
       </Page>
     );
